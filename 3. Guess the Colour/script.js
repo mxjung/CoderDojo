10aad35f74844fd19886f05257aa347757f3c375
Max Jung <jung.moonki94@gmail.com>
2019-12-09 22:26:42 -0800

    Added Event Listeners to all buttons and added reset button to rerun game without refreshing page

--- 3. Guess the Colour/script.js
+++ 3. Guess the Colour/script.js
@@ -1,13 +1,12 @@
 var heading = document.getElementById("colourValue");
 var buttons = document.getElementsByClassName("colourButton");
-var answerButton = Math.round(Math.random() * (buttons.length - 1));
-var answerMessage = document.getElementById('answer');
+var answerMessage = document.getElementById("answer");
 
 // Make element show
 // heading.innerHTML = "Hello world!";
 
-function setButtonColour(button, red, green, blue) {
-  button.setAttribute(
+function setButtonColour(buttons, red, green, blue) {
+  buttons.setAttribute(
     "style",
     "background-color: rgb(" + red + "," + green + "," + blue + ");"
   );
@@ -17,15 +16,35 @@ function makeColourValue() {
   return Math.round(Math.random() * 255);
 }
 
-for (let i = 0; i < buttons.length; i++) {
-  var red = makeColourValue();
-  var green = makeColourValue();
-  var blue = makeColourValue();
+function startGame() {
+  // Erase any existing header element
+  answerMessage.innerHTML = "";
 
-  setButtonColour(buttons[i], red, green, blue);
+  var answerButton = Math.round(Math.random() * (buttons.length - 1));
 
-  if (i === answerButton){
+  for (let i = 0; i < buttons.length; i++) {
+    var red = makeColourValue();
+    var green = makeColourValue();
+    var blue = makeColourValue();
+
+    setButtonColour(buttons[i], red, green, blue);
+
+    if (i === answerButton) {
       heading.innerHTML = `(${red}, ${green}, ${blue})`;
+    }
+
+    // Event Listener for Each Color Button
+    buttons[i].addEventListener("click", function() {
+      if (this === buttons[answerButton]) {
+        answerMessage.innerHTML = "Correct!";
+      } else {
+        answerMessage.innerHTML = "Wrong answer! Guess again!";
+      }
+    });
   }
 }
 
+startGame();
+
+// Event Listener to re-start game when reset button is clicked on
+document.getElementById("resetButton").addEventListener("click", startGame);
